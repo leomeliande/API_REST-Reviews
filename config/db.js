075bcb01f35e99f3eb81edafff71f8b54f64187a
leomeliande <35746765+leomeliande@users.noreply.github.com>
2019-09-21 06:04:29 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
 const dbURI =
-  "mongodb+srv://leomeliande:VDeOWJTeyhPkxxRy@review-pwact.mongodb.net/test?retryWrites=true&w=majority";
-
-retryWrites=true";
+  "mongodb+srv://leomeliande:VDeOWJTeyhPkxxRy@review-pwact.mongodb.net/test?retryWrites=true&w=majority retryWrites=true";
 
 mongoose.connect(dbURI, { 
     useNewUrlParser: true,
